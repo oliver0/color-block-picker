ee65c43bf8210371ebc9b6a411e2fa85b8555da8
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-02 09:18:04 -0500

    Add add button

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
-  var cssColorNames = ["Aqua","Black","Blue","Brown","Coral","Cyan","Grey","Green","Indigo","Ivory","Lavender","Lime","Magenta","Maroon","Navy","Olive","Orange","Pink","Plum","Purple","Red","Salmon","Silver","Teal","Turquoise","Violet","Yellow"];
+  var cssColorNames = ["Aqua","Black","Brown","Coral","Cyan","Grey","Indigo","Ivory","Lavender","Lime","Magenta","Maroon","Navy","Olive","Orange","Pink","Plum","Purple","Salmon","Silver","Teal","Turquoise","Violet"];
   var colorsArray = ['red', 'green', 'yellow', 'blue'];
-  for(i=0; i < colorsArray.length; i++){
-    $('#container').append('<div id=' + colorsArray[i] + ' class="square"></div>');
-  }
+
+
+  appendInitialColors();
+  $('body').append('<button>Add</button>');
   //$('#container').append('<div id="red" class="square"></div><div id="green" class="square"></div><div id="yellow" class="square"></div><div id="blue" class="square"></div>')
 
-  var currentColor = colorsArray[randomNumber(0, colorsArray.length)];
+  var currentColor = colorsArray[randomNumber(0, colorsArray.length-1)];
   var stringChoose = '';
   $('#title').text('Please pick the ' + currentColor + ' box!');
   $('#container').on('click', 'div', function () {
+
     var clickedColor = $(this).attr('id');
     if(clickedColor == currentColor) {
       alert("Nice! You picked wisely!");
-      currentColor = colorsArray[randomNumber(0, 3)];
+      var random = randomNumber(0, colorsArray.length-1);
+      currentColor = colorsArray[randomNumber(0, colorsArray.length-1)];
       stringChoose = 'Please pick the ' + currentColor + ' box!';
       $('#title').text(stringChoose);
       $('#message').empty();
@@ -22,10 +25,34 @@ $(document).ready(function () {
     }
 
   });
+  $('button').on('click', function(){
+    if(cssColorNames.length == 0){
+      alert('Sorry, no more colors!');
+    } else {
+    var random = randomNumber(0, cssColorNames.length-1);
+    var cssColor = cssColorNames.splice(random, 1);
+    //console.log(cssColor);
+    //console.log(random);
+    //var newColor = cssColorNames[randomNumber(0, cssColorNames.length)];
+    colorsArray.push(cssColor);
+    $('#container').append('<div id=' + cssColor + ' class="square"></div>');
+    $('#container').children().last().css('background-color', cssColor);
+    console.log(colorsArray.length, cssColorNames);
+    }
+
+  });
+
+  function appendInitialColors(){
+    for(i=0; i < colorsArray.length; i++){
+    $('#container').append('<div id=' + colorsArray[i] + ' class="square"></div>');
+    }
+  }
+
+  function randomNumber(min, max){
+      return Math.floor(Math.random() * (1 + max - min) + min);
+  }
 });
 
-function randomNumber(min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);
-}
+
 
 //console.log(randomNumber(0, 3));
