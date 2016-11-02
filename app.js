82359a06a2e488f7b6f1774550933e98e1dd6976
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-02 11:34:54 -0500

    Add delete button that works with some bugs

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
   var cssColorNames = ["Aqua","Black","Brown","Coral","Cyan","Grey","Indigo","Ivory","Lavender","Lime","Magenta","Maroon","Navy","Olive","Orange","Pink","Plum","Purple","Salmon","Silver","Teal","Turquoise","Violet"];
   var colorsArray = ['red', 'green', 'yellow', 'blue'];
-
-
   appendInitialColors();
-  $('body').append('<button>Add</button>');
-  //$('#container').append('<div id="red" class="square"></div><div id="green" class="square"></div><div id="yellow" class="square"></div><div id="blue" class="square"></div>')
+  $('body').append('<button id="add">Add</button>');
 
   var currentColor = colorsArray[randomNumber(0, colorsArray.length-1)];
   var stringChoose = '';
   $('#title').text('Please pick the ' + currentColor + ' box!');
-  $('#container').on('click', 'div', function () {
 
-    var clickedColor = $(this).attr('id');
+
+  // This is listening for a click on one of the squares. It checks to see if the class of the
+  // clicked color matches the currently chosen guessing color. If it matches, alerts winning message
+  // and sets current color to a randomly chosen one, resets the prompt message and empties #message
+  //
+
+  $('#container').on('click', 'div', function () {
+    var clickedColor = $(this).attr('class');
     if(clickedColor == currentColor) {
       alert("Nice! You picked wisely!");
       var random = randomNumber(0, colorsArray.length-1);
@@ -25,26 +28,43 @@ $(document).ready(function () {
     }
 
   });
-  $('button').on('click', function(){
+
+  // This is listening for a click on the delete button. If it is hit it removes the color from color array
+  // then pushes
+  $('#container').on('click', 'button', function () {
+    for(i=0; i <colorsArray.length; i++){
+      if(colorsArray[i]==$(this).attr('class')){
+        var removedColor = colorsArray.splice(i, 1);
+        cssColorNames.push(removedColor);
+      }
+    }
+    console.log(cssColorNames);
+    console.log(colorsArray);
+    $(this).parent().remove();
+  });
+
+  $('#add').on('click', function(){
     if(cssColorNames.length == 0){
       alert('Sorry, no more colors!');
     } else {
     var random = randomNumber(0, cssColorNames.length-1);
     var cssColor = cssColorNames.splice(random, 1);
-    //console.log(cssColor);
-    //console.log(random);
-    //var newColor = cssColorNames[randomNumber(0, cssColorNames.length)];
     colorsArray.push(cssColor);
-    $('#container').append('<div id=' + cssColor + ' class="square"></div>');
-    $('#container').children().last().css('background-color', cssColor);
-    console.log(colorsArray.length, cssColorNames);
+    $('#container').append('<div class="' + cssColor + '" square"><button class=' + cssColor + '>delete</button></div>');
+    $('#container').find('div').last().css({'background-color': cssColor, 'width': '100px', 'height': '100px', 'display': 'inline-block', 'margin': '20px'});    //$('#container').children().last().addClass(cssColor);
     }
 
   });
 
+
+
+
+
+
   function appendInitialColors(){
     for(i=0; i < colorsArray.length; i++){
-    $('#container').append('<div id=' + colorsArray[i] + ' class="square"></div>');
+    $('#container').append('<div class="' + colorsArray[i] + '" square"><button class=' + colorsArray[i] + '>delete</button></div>');
+    $('#container').find('div').last().css({'background-color': colorsArray[i], 'width': '100px', 'height': '100px', 'display': 'inline-block', 'margin': '20px'});
     }
   }
 
